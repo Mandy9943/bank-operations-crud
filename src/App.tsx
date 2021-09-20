4981e15e91e70877162640b742e4422fadd392e1
Cesar <cesrmartn@yahoo.es>
2021-09-20 13:35:51 +0200

    Update - name of app.css to App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Filter from "./components/FIlter/Filter";
-import "./app.css";
+import "./App.css";
 import "./assets/font-awesome/css/font-awesome.min.css";
 function App() {
   return <Filter />;
